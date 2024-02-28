aa277527dad4482a1556eebca9c49ca276ce4894
sebbex1337 <sefort@hotmail.dk>
2024-02-28 11:04:21 +0100

    fetch

--- src/exercises/FetchDemo.tsx
+++ src/exercises/FetchDemo.tsx
@@ -38,7 +38,7 @@ export default function FetchDemo1({ title }: BaseProps) {
             setUser(response);
             console.log(response);
         });
-    }, []);
+    }, [userId]);
 
     return (
         <>
